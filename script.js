16cb6a43cc9958d157fbe5a7fbaa35241bd7ae72
2tvto147 <2tvto147@gmail.com>
2025-08-11 02:53:46 -0700

    react app + gh pages

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeText() {
-  document.getElementById("dynamic-text").innerText = "You clicked the button!";
-}
-
-document.getElementById("goButton").addEventListener("click", function() {
-    window.location.href = "https://random-restart.vercel.app/Software-Documentation/Web-Development/Web-Dev-101";
-});
